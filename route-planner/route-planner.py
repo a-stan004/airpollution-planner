ed96ddac03d261571c8ebb8de1c1f453cded48c2
a-stan004 <164003865+a-stan004@users.noreply.github.com>
2024-04-22 20:05:15 +0100

    solve - attempt #2 rewritten functions
    rewritten functions to break down while loops, now functions as intended.
    Integrated cache to speed up solver

--- route-planner/route-planner.py
+++ route-planner/route-planner.py
@@ -1,27 +1,24 @@
 import osmnx as ox
 import networkx as nx
 import geopandas as gpd
-# import matplotlib.pyplot as plt
 import pandas as pd
 from geopy import Nominatim
 from networkx import NetworkXNoPath
-
-from airpollutionAPI import PointQuality
 from raster import obtainvalue
 
 # Safe limits for air pollution 2021 and 2005 WHO
 WHO2025 = {
-    "pm2.5": 1,
+    "pm2_5": 1,
     "pm10": 5,
     "no2": 3
 }
 WHO2021 = {
-    "pm2.5": 5,
+    "pm2_5": 5,
     "pm10": 15,
     "no2": 10
 }
 WHO2005 = {
-    "pm2.5": 10,
+    "pm2_5": 10,
     "pm10": 20,
     "no2": 40
 }
@@ -80,7 +77,7 @@ class Inputs:
 
 
 # Creates class instance of Inputs with two user inputs
-userinputs = Inputs("London Paddington", "London Euston")
+userinputs = Inputs("Wembley Stadium", "Tower Bridge")
 
 geo_initial = userinputs.geocodeaddresses()[0]
 geo_target = userinputs.geocodeaddresses()[1]
@@ -173,76 +170,155 @@ box = gdf.unary_union.envelope
 buffbox = box.buffer(0.01)
 
 # Constructing the graph using OSMnx
-graph = ox.graph_from_polygon(buffbox, network_type='walk', truncate_by_edge=False, retain_all=True)
+graph = ox.graph_from_polygon(buffbox, network_type='bike', truncate_by_edge=False, retain_all=True)
 
-# ==========================================================================
-# Drawing route between locations, check pollution values and redraw
-# ==========================================================================
+# Getting inital user locations and drawing initial graph and route
+usernodes = userlocations.getnodes()
+route = nx.shortest_path(G=graph, source=usernodes[0], target=usernodes[1], weight="distance")
+fig, ax = ox.plot_graph_route(graph, route)
 
-pm2_5value = float(WHO2005["pm2.5"])
+# Get initial limiter values
+print("Start path finding ...")
+pm2_5value = float(WHO2005["pm2_5"])
 pm10value = float(WHO2005["pm10"])
 no2value = float(WHO2005["no2"])
-bounds = ox.graph_from_polygon(buffbox, network_type='walk', truncate_by_edge=False, retain_all=True)
-nodes, edges = ox.graph_to_gdfs(bounds, nodes=True, edges=True)
-usernodes = userlocations.getnodes()
 
-ogroute = nx.shortest_path(G=bounds, source=usernodes[0], target=usernodes[1], weight='distance')
-fig, ax = ox.plot_graph_route(bounds, ogroute)
+# Collect into tuple and define inital tolerance
+limits = (pm2_5value, pm10value, no2value)
+tolerance = 1
 
-updatedroute = nx.shortest_path(G=bounds, source=usernodes[0], target=usernodes[1], weight='distance')
-nodecount = 1
+# Set up a location to store pollution values that are high
+geo_cache = {}
 
-
-def removenodes(bounds, newroute, p_pm2_5value, p_pm10value, p_no2value, count):
-    pollution_status = False
-    if count >= 1:
-        count = 0
-
-    while not pollution_status:
-
-        route_nodes = nodes.loc[newroute]
-        print("Route made")
-
-        for index, row in route_nodes.iterrows():
-            x_coord = row['x']
-            y_coord = row['y']
-            pm2_5 = obtainvalue(y_coord, x_coord, "pm2.5")
-            pm10 = obtainvalue(y_coord, x_coord, "pm10")
-            no2 = obtainvalue(y_coord, x_coord, "no2")
-
-            if index != usernodes[0] and index != usernodes[1]:
-                if pm2_5 > p_pm2_5value or pm10 > p_pm10value or no2 > p_no2value:
-                    bounds.remove_node(index)
-                    print("Node removed")
-                    count += 1
-
-        pollution_status = True
-        if nodecount >= 1:
-            successful = False
-        else:
-            successful = True
-
-    return bounds, nodecount, successful
+# Take graph and make it into a geodataframe
+geo_data = ox.graph_to_gdfs(graph, nodes=True, edges=False)
 
 
-success = False
+def get_location(node):
+    """
+    Takes a node as input and returns its x and y values
 
-while not success:
+    Args
+        node (dict): OSMnx type node
+    Returns
+        x (int): Value from x field of node dataframe
+        y (int): Value from y field of node dataframe
+    """
+    row = geo_data.loc[node]
+    x = row.x
+    y = row.y
+    return x, y
+
+
+def get_geo_data(node):
+    """
+    Takes a node as input and gathers pollution data about node, uses obtainvalue function from raster script
+    Requires geocache dictionary to be setup prior i.e. geo_cache={}
+
+    Args
+        node (dict): OSMnx type node
+    Returns
+        node (dict): Node added directly to geocache
+    """
+    if node not in geo_cache.keys():
+        x, y = get_location(node)
+        geo_cache[node] = (
+            obtainvalue(y, x, "pm2.5"),
+            obtainvalue(y, x, "pm10"),
+            obtainvalue(y, x, "no2"),
+        )
+    return geo_cache[node]
+
+
+def compare(values, limiters):
+    """
+    Compares node pollution values to limits
+
+    Args
+        values (dict): OSMnx node dictionary
+    Returns
+        within (bool): Returns true if all values are within limits
+    """
+    within = all(value < limiter * tolerance for value, limiter in zip(values, limiters))
+    return within
+
+
+# Unordered sets created to store nodes
+all_nodes = set(graph.nodes())
+bad_nodes = set()
+
+
+def good_node(node):
+    """
+    Checks whether a node is a start or finish node (automatically skipped), and if it is within allowed limits
+
+    Args
+        node (dict): OSMnx node dictionary
+    Returns
+        goodbool (bool): Returns true if value is within limits or start/end node
+    """
+    if node in usernodes:
+        return True
+    goodbool = compare(get_geo_data(node), limits)
+    return goodbool
+
+
+def process_path(path):
+    """
+    Checks all the nodes in a route for nodes exceeding limits, and adds these to a list of bad nodes
+    Requires creation of an empty set for bad nodes i.e. bad_nodes = set()
+
+    Args
+        path (list): List of node dictionaries as OSMnx route
+    Returns
+        good_path (bool): Returns true if all values are within limits
+    """
+    good_path = True
+    for node in path:
+        if not good_node(node):
+            bad_nodes.add(node)
+            good_path = False
+    return good_path
+
+
+def restricted_path(res_graph):
+    """
+    Tries to constuct a path with the high pollution value nodes removed.
+    Where this is not possible a False statement is returned
+
+    Args
+        res_graph (MultiDiGraph): OSMnx pre-built graph as input
+    Returns
+        short_path (list): List of node dictionaries as OSMnx route if successful or False
+    """
+    nodes = all_nodes - bad_nodes
+    sub = nx.subgraph(res_graph, nodes)
     try:
-        while nodecount != 0:
-            boundsremoved = removenodes(bounds, updatedroute, pm2_5value, pm10value, no2value, nodecount)[0]
-            updatedroute = nx.shortest_path(G=boundsremoved, source=usernodes[0], target=usernodes[1], weight='distance')
-            print("Route attempt")
-            success = removenodes(bounds, updatedroute, pm2_5value, pm10value, no2value, nodecount)[2]
+        short_path = nx.shortest_path(sub, source=usernodes[0], target=usernodes[1], weight="distance")
+        return short_path
     except NetworkXNoPath:
-        pm2_5value *= 1.5
-        pm10value *= 1.5
-        no2value *= 1.5
-        bounds = ox.graph_from_polygon(buffbox, network_type='walk', truncate_by_edge=False, retain_all=True)
-        nodes, edges = ox.graph_to_gdfs(bounds, nodes=True, edges=True)
-        print("Error, redrawing graph and upping bounds")
-        print(pm2_5value, pm10value, no2value)
+        return False
 
-print("Found route")
-print(pm2_5value, pm10value, no2value)
-fig, ax = ox.plot_graph_route(bounds, updatedroute)
+
+# Attempts to make a valid path
+valid_path = process_path(route)
+# route_possible = True
+
+# Counter for how many attempts
+counter = 1
+attempt = []
+
+# Where valid path has not been found, while loop reattempts route by resetting bad nodes and upping limit tolerances
+while not valid_path:
+    print(f"Attempt: {counter}, tolerance:{tolerance}")
+    attempt = restricted_path(graph)
+    counter += 1
+    if not attempt:
+        tolerance *= 1.5
+        bad_nodes = set()
+    else:
+        valid_path = process_path(attempt)
+
+print("Path found")
+fig, ax = ox.plot_graph_route(graph, attempt)
+
